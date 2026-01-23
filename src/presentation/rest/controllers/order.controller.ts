e90c43f90f6c4790c4449d5d4fbc98ad860c8fa7
kardasch404 <zz2406143@gmail.com>
2026-01-23 11:26:50 +0100

    feat(order): add REST API controller

---
+++ src/presentation/rest/controllers/order.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Post, Param, Body, Query } from '@nestjs/common';
+import { CreateOrderUseCase } from '../../../application/use-cases/commands/create-order.use-case';
+import { CancelOrderUseCase } from '../../../application/use-cases/commands/cancel-order.use-case';
+import { GetOrderQueryHandler, GetOrderQuery } from '../../../application/use-cases/queries/get-order.query';
+import { GetUserOrdersQueryHandler, GetUserOrdersQuery } from '../../../application/use-cases/queries/get-user-orders.query';
+import { CreateOrderCommand } from '../../../application/dto/request/create-order.command';
+import { CancelOrderCommand } from '../../../application/dto/request/cancel-order.command';
+
+@Controller('api/orders')
+export class OrderController {
+  constructor(
+    private readonly createOrderUseCase: CreateOrderUseCase,
+    private readonly cancelOrderUseCase: CancelOrderUseCase,
+    private readonly getOrderQuery: GetOrderQueryHandler,
+    private readonly getUserOrdersQuery: GetUserOrdersQueryHandler,
+  ) {}
+
+  @Post()
+  async createOrder(@Body() body: any) {
+    const command = new CreateOrderCommand(
+      body.userId,
+      body.shippingAddress,
+      body.billingAddress,
+      body.notes,
+    );
+    return await this.createOrderUseCase.execute(command, body.cart);
+  }
+
+  @Get()
+  async getUserOrders(@Query('userId') userId: string, @Query('page') page = 1, @Query('limit') limit = 20) {
+    const query = new GetUserOrdersQuery(userId, page, limit);
+    return await this.getUserOrdersQuery.execute(query);
+  }
+
+  @Get(':id')
+  async getOrder(@Param('id') id: string) {
+    const query = new GetOrderQuery(id);
+    return await this.getOrderQuery.execute(query);
+  }
+
+  @Post(':id/cancel')
+  async cancelOrder(@Param('id') id: string, @Body() body: any) {
+    const command = new CancelOrderCommand(id, body.userId, body.reason);
+    await this.cancelOrderUseCase.execute(command);
+    return { message: 'Order cancelled successfully' };
+  }
+}
